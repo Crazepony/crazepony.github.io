eb780322564a134ff456d1c079c215fa029b0eeb
nieyong <nieyong_1947@126.com>
2015-04-16 16:00:50 +0800

    + 添加bootstrap的affix插件固定sidenav

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -72,6 +72,24 @@ $(document).ready(function(){
 		$(".sidebar-container").append(html);
 	}
 
+    // Sidenav affixing
+    var $sideBar = $('.bs-docs-sidebar')
+
+    $sideBar.affix({
+    offset: {
+      top: function () {
+        var offsetTop      = $sideBar.offset().top
+        var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+        var navOuterHeight = $('.bs-docs-nav').height()
+
+        return (this.top = offsetTop - navOuterHeight - sideBarMargin)
+      },
+      bottom: function () {
+        return (this.bottom = $('.bs-docs-footer').outerHeight(true))
+      }
+    }
+    });
+
 });
 
 
